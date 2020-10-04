282642e6b61f51800ae73e4277611f315023537f
gardas86@gmail.com <gardas86@gmail.com>
2020-10-04 19:24:47 +0200

    Parameter.m added, Simulink Quadcopter physical model created

---
+++ Parameters.m
@@ -0,0 +1,16 @@
+%Total weight
+m = 2.2;
+%%Gravitational acceleration
+g= -9.81;
+%Ground level [m]
+ground_level = -5;
+%Moments of inertia
+Ixx = 0.2;
+Iyy = 0.2;
+Izz = 0.2;
+%Initial position:
+Pos_init = [0;0;0];
+%Initial velocity:
+Vel_init= [0;0;0];
+
+
