09d84c4a94a16e989862b42f947e825edceb8a7b
gardas86@gmail.com <gardas86@gmail.com>
2020-10-06 00:57:10 +0200

    Working Altitude-Attitude PID controller added

--- Parameters.m
+++ Parameters.m
@@ -7,6 +7,7 @@ ground_level = 0;
 %% Quadcopter properties
 % Total weight
 m = 2.2;
+gm = [0;0;g*m];
 % Moments of inertia
 Ixx = 0.2;
 Iyy = 0.2;
@@ -18,31 +19,34 @@ Vel_init= [0;0;0];
 %Motor-mass center distance [m]
 epsilon = 0.22;
 % lift coefficient
-cl = 0.2; % F = w^2*cl
+cl = 0.0000002; % F = w^2*cl
 % drag coefficient
-cd = 0.8; % M=w^2*cd
+cd = 0.00000008; % M=w^2*cd
 % Rotational inertia of propeller
 Jprop = 0.2;
+%Maximum value of Revolutions per min
+maxRPM = 10000;
+maxLiftForce = maxRPM^2 * cl;
 
 %% Sensor properties
 gyro_update_rate = 0.01; 
 
 %% Actuator properties
-A = [0 0 epsilon -epsilon;
-    epsilon -epsilon 0 0;
-    cd/cl -cd/cl cd/cl -cd/cl;
-    1 1 1 1];
+A = [1 1 1 1;
+0 -epsilon 0 epsilon;
+epsilon 0 -epsilon 0;
+-cd/cl cd/cl -cd/cl cd/cl];
 
 P = [0 0 0 0;
     0 0 0 0;
-	Jprop -Jprop -Jprop Jprop;
-    0 0 0 0];
+    0 0 0 0
+    Jprop -Jprop -Jprop Jprop];
 
+
+% THIS MUST BE USED BECAUSE this form works only with the LU SOLVER
 actuator_calculator = [
 1 1 1 1;
 0 -epsilon 0 epsilon;
 epsilon 0 -epsilon 0;
--cd/cl cd/cl -cd/cl cd/cl
-]
-det(actuator_calculator)
+-cd/cl cd/cl -cd/cl cd/cl]
 
